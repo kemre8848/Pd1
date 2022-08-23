fb739b428e13425c367d179780bdaa5600e78a5c
kemre8848 <97333459+kemre8848@users.noreply.github.com>
2022-08-23 21:20:17 +0300

    Add files via upload

---
+++ lab8s2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int r,pi=3,alan;
+    printf("sayi giriniz/n");
+    scanf("%d",&r);
+    alan=r*r*pi;
+    printf("alan  = %d",alan);
+    return 0;
+}
